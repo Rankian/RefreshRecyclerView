065746dfc4e981065fbb3e06e328339b378e1e1a
LangSanJie <langsj637@gmail.com>
2017-02-10 10:28:31 +0800

    SanJie

---
+++ refresh-recyclerview/src/main/java/com/sanjie/refresh/view/BaseRefreshHeader.java
@@ -0,0 +1,19 @@
+package com.sanjie.refresh.view;
+
+/**
+ * Created by SanJie on 17/2/9.
+ */
+interface BaseRefreshHeader {
+
+	int STATE_NORMAL = 0;
+	int STATE_RELEASE_TO_REFRESH = 1;
+	int STATE_REFRESHING = 2;
+	int STATE_DONE = 3;
+
+	void onMove(float delta);
+
+	boolean releaseAction();
+
+	void refreshComplete();
+
+}
